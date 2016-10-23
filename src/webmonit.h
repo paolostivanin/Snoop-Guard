f41570de6010e34612c2d8eff2a845e69d831705
Paolo Stivanin <info@paolostivanin.com>
2016-10-23 21:24:04 +0200

    updates

--- src/webmonit.h
+++ src/webmonit.h
@@ -7,10 +7,16 @@ extern struct _devs {
 
 struct _devs *list_webcam (void);
 
-int is_dev_being_used (int fd, const char *dev_name);
+int get_webcam_status (int fd, const char *dev_name);
 
 int xioctl (int fh, unsigned long request, void *arg);
 
 void init_device (int fd, const char *dev_name);
 
 int open_device (const char *dev_name);
+
+int set_nonblock (int fd);
+
+int server_mode (void);
+
+int client_mode (void);
