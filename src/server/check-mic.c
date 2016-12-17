04df4b8d7d5d879359ceb4bbd466dbba48087d4d
Paolo Stivanin <info@paolostivanin.com>
2016-12-17 19:03:58 +0100

    working on getting mic device

--- src/server/check-mic.c
+++ src/server/check-mic.c
@@ -1,16 +1,21 @@
 #define _GNU_SOURCE
 #include <glib.h>
+#include <gio/gio.h>
 #include <alsa/asoundlib.h>
 #include "main.h"
+
+
+gint check_sysdefault_dev ();
 
 
 gint
 get_mic_status (const gchar *mic)
 {
-    // TODO: grep capture /proc/asound/devices. If 1 then sysdefault? And if 2 what? DEAL WITH MULTIPLE MIC
-    /* TODO arecord -L | grep -w sysdefault:CARD with system() could be a solution. Check the fork() thing to add more security
-     * (https://www.securecoding.cert.org/confluence/pages/viewpage.action?pageId=2130132)
-     */
+    if (g_strcmp0 (mic, "sysdefault") == 0) {
+        // TODO check error
+        check_sysdefault_dev ();
+    }
+
     snd_pcm_t *capture_handle = NULL;
 
     if (snd_pcm_open (&capture_handle, mic, SND_PCM_STREAM_CAPTURE, 0) < 0) {
@@ -20,5 +25,32 @@ get_mic_status (const gchar *mic)
     else {
         snd_pcm_close (capture_handle);
         return MIC_NOT_IN_USE;
+    }
+}
+
+
+gint
+check_sysdefault_dev ()
+{
+    GError *err = NULL;
+    GSubprocess *sp = g_subprocess_new (G_SUBPROCESS_FLAGS_STDOUT_PIPE, &err, "/usr/bin/arecord", "-L", NULL);
+    if (sp == NULL) {
+        g_print ("%s\n", err->message);
+        g_clear_error (&err);
+        return GENERIC_ERROR;
+    }
+
+    gchar *output = NULL;
+    if (!g_subprocess_communicate_utf8 (sp, NULL, NULL, &output, NULL, &err)) {
+        g_print ("%s\n", err->message);
+        g_clear_error (&err);
+        return GENERIC_ERROR;
+    }
+    if (g_strstr_len (output, -1, "sysdefault:CARD") == NULL) {
+        g_free(output);
+        return SYSDEFAULT_FOUND;
+    } else {
+        g_free(output);
+        return SYSDEFAULT_DEV_NOT_FOUND;
     }
 }
